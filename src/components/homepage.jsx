8a94145457a8571220bd53f7cbcc87fcac6bd84e
Lsiopongco <58718096+luigisiops@users.noreply.github.com>
2021-01-08 05:46:40 -0500

    new files

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -14,26 +14,19 @@ import Python from '../css/images/languages/python.png'
 import Django from '../css/images/languages/django.png'
 import Psql from '../css/images/languages/psql.png'
 
-
+import Gradpic from '../css/images/gradpic.png'
 
 import Gohort from '../css/images/Gohort.png'
 import Chalkup from '../css/images/chalkup.png'
 import Flixlists from '../css/images/flixlists.png'
 import Gifter from '../css/images/Gifter.png'
+import silverthread from '../css/images/silverthread.png'
+import socialbubble from '../css/images/social-bubbles.png'
 
 
-const Navbar = () => {
+const Headers = (three) => {
     return(
-    <div id= "navbar">
-        <button id = "home" className="nav-element">Home</button>
-        <button className="nav-element">About</button>
-        <button className="nav-element">Projects</button>
-    </div>
-    )}
-
-const Headers = () => {
-    return(
-    <div id = "title-socials-container">
+    <div ref = {three.three} id = "title-socials-container">
         <img id = "profile-pic" src={profilepic}/>
         <h1 id="title">Luigi Siopongco</h1>
         <div id="descriptors">Fullstack Developer | Writer | Photographer </div>
@@ -44,12 +37,12 @@ const Headers = () => {
             <a href = "https://github.com/luigisiops" className="socials"><FontAwesomeIcon icon = {faGithub}/></a>
             <a href = "https://medium.com/@luigisiops"className="socials"><FontAwesomeIcon icon = {faMedium}/></a>
         </div>
-        <a href='./altresume.pdf' download> Download Resume</a>
+        <a href='./Siopongco_Resume.pdf' download> Download Resume</a>
     </div>
     
     )}
 
-const About = () => {
+const About = (two) => {
         const [isCssandHtml, setCssandHtml] = useState(false)
         const [isJavascript, setJavascript] = useState(false)
         const [isNode, setNode] = useState(false)
@@ -66,39 +59,44 @@ const About = () => {
             )
         })
     return(
-    <div id = "about">
+    <div ref = {two.two} id = "about">
+        <img src = {Gradpic} id = "grad-picture"/>
         <div id = "about-information">
             <h1 id = "about-title">Hi I'm Luigi. Nice to meet you!</h1>
             <p id = "about-statement">As an up-and-coming developer, I am on a continuous journey of self-improvement. 
-                I have worked in fast-paced tech environments, collaborated to develop applications, and led successful projects/events.</p>
+                I have worked in fast-paced environments, collaborated to develop applications, and led successful projects/events.</p>
             <h1 id = "about-language-title">Known Languages / Tools</h1>
             <div id = "about-language-icons">
                 {icons}              
             </div>
         </div>   
+
     </div>
     )}
 
-const Project = () => {
+const Project = (one) => {
     const [isProjectOne, setProjectOne] = useState(false)
     const [isProjectTwo, setProjectTwo] = useState(false)
     const [isProjectThree, setProjectThree] = useState(false)
     const [isProjectFour, setProjectFour] = useState(false)
+    const [isProjectFive, setProjectFive] = useState(false)
+    const [isProjectSix, setProjectSix] = useState(false)
 
 
     return(
-        <div id = "projects">
+        <div ref = {one.one}id = "projects">
             <div id = "project-header">Projects</div>
 
             <div id = "project-content">
                 <div className = "project-upper">
+
                     <div onMouseEnter = {() => setProjectOne(true)} onMouseLeave = {() => setProjectOne(false)} className = "project-item">
                         <img className = "project-image" src = {Gohort}/>
                             {isProjectOne && (
                                 <div className = "top">
                                     <div className = "show">Gohort</div>
-                                    <div className = "project-body">A travel planning application</div>
-                                    <a href = "https://github.com/luigisiops" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
+                                    <div className = "project-body">A team built travel planning application with multiple features including, payments, locations, saving plans, etc. Built with React/Redux and Django </div>
+                                    <a href = "https://github.com/chan-labs/trippy-api" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
                                 </div>
                             )}
                     </div>
@@ -112,26 +110,24 @@ const Project = () => {
                                         An application for rock climbing enthusiasts built with MustacheJs on the frontend
                                         and NodeJS and Postgres SQL on the backend
                                     </div>
-                                    <a href = "https://github.com/luigisiops" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
+                                    <a href = "https://github.com/luigisiops/CHALK-UP" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
                                 </div>
                             )}
                     </div>
-                </div>
 
-                <div className = "project-lower">
                     <div onMouseEnter = {() => setProjectThree(true)} onMouseLeave = {() => setProjectThree(false)} className = "project-item">
                         <img className = "project-image" src = {Flixlists}/>
                             {isProjectThree && (
                                 <div className = "top">
                                     <div className = "show">Flixlists</div>
                                     <div className = "project-body">
-                                        A front end project made for learning frontend web development using a movie web api. Users are able to search and create
+                                        A full stack project made with Javascript and Firebase using a movies REST api. Users are able to search and create
                                         personal movie lists which was stored on firebase in the backend.
                                     </div>
-                                    <a href = "https://github.com/luigisiops" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
+                                    <a href = "https://github.com/luigisiops/FlixLists" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
                                 </div>
                             )}
-                    </div>
+                    </div>                    
 
                     <div  onMouseEnter = {() => setProjectFour(true)} onMouseLeave = {() => setProjectFour(false)} className = "project-item">
                         <img className = "project-image" src = {Gifter}/>
@@ -139,9 +135,36 @@ const Project = () => {
                                 <div className = "top">
                                     <div className = "show">GIFter</div>
                                     <div className = "project-body">
-                                        A fun small frontend react project that allows a user to search for gifs via the tenor api and save their favorite ones.
+                                        A fun small frontend react project that allows a user to search for gifs via the tenor api. Built with React.
                                     </div>
                                     <a href = "https://github.com/luigisiops" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
+                                </div>
+                            )}
+                    </div>
+
+                    <div  onMouseEnter = {() => setProjectFive(true)} onMouseLeave = {() => setProjectFive(false)} className = "project-item">
+                        <img className = "project-image" src = {socialbubble}/>
+                            {isProjectFive && (
+                                <div className = "top">
+                                    <div className = "show">Social Bubble</div>
+                                    <div className = "project-body">
+                                        A fullstack application built for the 2020 Developers Week Hack NYC Hackathon. This is an socialmedia app for keeping 
+                                        track of social groups throughout quarantine. Built with React/Redux on the frontend and Node on the backend.
+                                    </div>
+                                    <a href = "https://github.com/luigisiops/social-bubble-client" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
+                                </div>
+                            )}
+                    </div>
+
+                    <div  onMouseEnter = {() => setProjectSix(true)} onMouseLeave = {() => setProjectSix(false)} className = "project-item">
+                        <img className = "project-image" src = {silverthread}/>
+                            {isProjectSix && (
+                                <div className = "top">
+                                    <div className = "show">Silver Thread Designs Inventory Management</div>
+                                    <div className = "project-body">
+                                        A freelance project built for a client that needed an inventory management system for her jewelry business. Built with React/Redux on the frontend and Node on the backend.
+                                    </div>
+                                    <a href = "https://github.com/luigisiops/silver-thread-client" className="project-github"><FontAwesomeIcon icon = {faGithub}/></a>
                                 </div>
                             )}
                     </div>
@@ -172,12 +195,20 @@ const Footer = () => {
 }
 
 const HomePageContainer = () => {
+const homeScroll = React.createRef()
+const aboutScroll = React.createRef()
+const projectScroll = React.createRef()
+const Scroll = (ref) => ref.current.scrollIntoView({behavior: "smooth"})
     return(
         <div id = 'webpage-container'>
-            <Navbar/>
-            <Headers/>
-            <About/>
-            <Project/>
+        <div id= "navbar">
+            <button onClick = {() => Scroll(homeScroll)} id = "home" className="nav-element">Home</button>
+            <button onClick = {() => Scroll(aboutScroll)} className="nav-element">About</button>
+            <button onClick = {() => Scroll(projectScroll)} className="nav-element">Projects</button>
+        </div>
+            <Headers three = {homeScroll}/>
+            <About two = {aboutScroll}/>
+            <Project one = {projectScroll}/>
             <ContactMe/>
             <Footer/>
         </div>
